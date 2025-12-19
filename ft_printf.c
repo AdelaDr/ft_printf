089b53418b6c7b82263d999494b1dcd9c797c138
AdelaDr <drahotovaadela@seznam.cz>
2025-12-19 15:51:06 +0100

    skeleton

--- ft_printf.c
+++ ft_printf.c
@@ -10,4 +10,56 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdarg.h>
+#include <unistd.h>
 
+static int type_parser(char type, va_list *args)
+{
+    if (type == 'c')
+        //return (ft_printf_c(args));
+    if (type == 's')
+        //return (ft_printf_s(args));
+    if (type == 'p')
+        //return (ft_printf_p(args));
+    if (type == 'd' || type == 'i')
+        //return (ft_printf_int(args));
+    if (type == 'u')
+        //return (ft_printf_u(args));
+    if (type == 'x')
+        //return (ft_printf_x(args));
+    if (type == 'X')
+        //return (ft_printf_X(args));
+    return (0);
+}
+
+int ft_printf(const char *format, ...)
+{
+    va_list args;
+    int count;
+    
+    count = 0;
+    va_start(args, format);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+            else 
+                if (*format)
+                    count += type_parser(*format, &args);
+        }
+        else
+        {
+            write(1, format, 1);
+            count++;
+        }
+        format++;
+    }
+    va_end(args);
+    return (count);
+}
