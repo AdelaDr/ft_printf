bcb460feedea6fbba289934a7169bff632abc026
AdelaDr <drahotovaadela@seznam.cz>
2025-12-20 01:14:18 +0100

    actual code

---
+++ ft_printf_str.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrahoto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 18:33:34 by adrahoto          #+#    #+#             */
+/*   Updated: 2025/12/16 18:33:54 by adrahoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <unistd.h>
+#include "libftprintf.h"
+
+int ft_printf_str(va_list *args)
+{
+    char *str;
+    int count;
+
+    count = 0;
+    str = va_arg(*args, char *);
+    if (!str)
+    return (write(1, "(null)", 6));
+    while (*str)
+    {
+        write(1, str, 1);
+        count++;
+        str++;
+    }
+    return (count);
+}
