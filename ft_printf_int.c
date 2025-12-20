b3d73e5101c20cf85b7e3d781acd19084d6f972f
AdelaDr <drahotovaadela@seznam.cz>
2025-12-20 01:11:21 +0100

    first version

---
+++ ft_printf_int.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrahoto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 18:33:34 by adrahoto          #+#    #+#             */
+/*   Updated: 2025/12/16 18:33:54 by adrahoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <unistd.h>
+#include "libftprintf.h"
+
+static int	ft_putnbr(int nb)
+{
+	char	a;
+    int count;
+
+    count = 0;
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return (11);
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+        count++;
+	}
+	if (nb >= 10)
+        count += ft_putnbr(nb / 10);
+	a = nb % 10 + '0';
+	write(1, &a, 1);
+    count++;
+    return(count);
+}
+
