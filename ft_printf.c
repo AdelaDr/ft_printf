90f11a1db94f20e68c93f10fbeab7acd54deba45
AdelaDr <133956939+AdelaDr@users.noreply.github.com>
2026-02-24 21:44:37 +0100

    norm + shorter fts

--- ft_printf.c
+++ ft_printf.c
@@ -10,57 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 #include <stdarg.h>
 #include <unistd.h>
-#include "libftprintf.h"
 
-static int type_parser(char type, va_list *args)
+static int	type_parser(char type, va_list *args)
 {
-    if (type == 'c')
-        return (ft_printf_char(args));
-    if (type == 's')
-        return (ft_printf_str(args));
-    if (type == 'p')
-        return (ft_printf_address(args));
-    if (type == 'd' || type == 'i')
-        return (ft_printf_int(args));
-    if (type == 'u')
-        return (ft_printf_unsigned(args));
-    if (type == 'x')
-        return (ft_printf_hex(args));
-    if (type == 'X')
-        return (ft_printf_hexupper(args));
-    return (0);
+	if (type == 'c')
+		return (ft_printf_char(args));
+	if (type == 's')
+		return (ft_printf_str(args));
+	if (type == 'p')
+		return (ft_printf_address(args));
+	if (type == 'd' || type == 'i')
+		return (ft_printf_int(args));
+	if (type == 'u')
+		return (ft_printf_unsigned(args));
+	if (type == 'x')
+		return (ft_printf_hex(args));
+	if (type == 'X')
+		return (ft_printf_hexupper(args));
+	return (0);
 }
 
-int ft_printf(const char *format, ...)
+static int	write_char(const char *c, int count)
 {
-    va_list args;
-    int count;
-    
-    count = 0;
-    va_start(args, format);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '%')
-            {
-                write(1, "%", 1);
-                count++;
-            }
-            else 
-                if (*format)
-                    count += type_parser(*format, &args);
-        }
-        else
-        {
-            write(1, format, 1);
-            count++;
-        }
-        format++;
-    }
-    va_end(args);
-    return (count);
+	write(1, c, 1);
+	count++;
+	return (count);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		count;
+
+	count = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else if (*format)
+				count += type_parser(*format, &args);
+		}
+		else
+			count = write_char(format, count);
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
